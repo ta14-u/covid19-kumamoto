8515e5c605bb31ea9b9bfd7344ae52c4143b6a16
iwaoka <33437483+iwaoka@users.noreply.github.com>
2020-03-27 11:39:21 +0900

    Update SideNavigation.vue

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -192,7 +192,6 @@ export default {
   }
 }
 </script>
-
 <style lang="scss" scoped>@media screen and (max-width: 600px){.SideNavigation-Logo{width:auto !important;}.SideNavigation-Logo img {width: 70px;}}.SideNavigation {
   position: relative;
   height: 100%;
